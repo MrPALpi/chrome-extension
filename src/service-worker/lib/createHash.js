8b309e216f5fa44e3da3e95330e10f90ae828343
Александр Капралов <kapralov.aleksandr@bodysite.ru>
2024-11-28 16:50:08 +0500

    улучшил hash-функцию

--- src/service-worker/lib/createHash.js
+++ src/service-worker/lib/createHash.js
@@ -2,11 +2,12 @@ export const createHash = async (text) => {
 
   return await new Promise((resolve) => {
     let hash = 0;
+    const maxHash = Math.pow(2, 32);
     for (let i = 0; i < text.length; i++) {
       const charCode = text.charCodeAt(i);
-      hash += charCode * (i + 1);
+      hash = (hash * 31 + charCode) % maxHash;
     }
 
-    resolve(hash.toString());
+    resolve(hash.toString(16));
   })
 }
